179676882af16e50217b65ff82cb2b4ed32b909a
Jay Kumar Gupta <jayesh.gupta3224@gmail.com>
2025-06-03 23:01:27 +0530

    solved merge sorted arrays

--- src/Leetcode/Arrays/Easy/MergeSortedArray.java
+++ src/Leetcode/Arrays/Easy/MergeSortedArray.java
@@ -4,26 +4,29 @@ import java.util.Arrays;
 
 public class MergeSortedArray {
     public static void main(String[] args) {
-        int[] nums1 = {3,4,5, 0, 0, 0}, nums2 = {1,2,3};
-        int m = nums1.length, n = nums2.length;
+        int[] nums1 = {1, 2, 3, 0, 0, 0}, nums2 = {1, 2, 3};
+        int m = 3, n = nums2.length;
         merge(nums1, m, nums2, n);
         System.out.println(Arrays.toString(nums1));
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-         int n1 = m-n-1,n2 = n-1;
-         int index = m-1;
-        for (int i = 0; i < n; i++) {
-            if(nums1[i]>nums2[i]){
-                nums1[index] = nums1[i];
-//                swap
-                nums1[i] = nums2[i];
+        int n1 = m - 1, n2 = n - 1;
+        int index = m + n - 1;
+        while (n1 >= 0 && n2 >= 0) {
+            if (nums1[n1] > nums2[n2]) {
+                nums1[index] = nums1[n1];
+                index--;
+                n1--;
+            } else {
+                nums1[index] = nums2[n2];
+                index--;
+                n2--;
             }
-            else{
-                nums1[index] = nums2[i];
-            }
+        }
+        while (n2 >= 0) {
+            nums1[index] = nums2[n2];
             index--;
-            n1--;
             n2--;
         }
 
