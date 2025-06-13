286c5aee7b1e98049e016403e1ee7c4c681aa64f
Jay Kumar Gupta <jayesh.gupta3224@gmail.com>
2025-06-13 22:27:12 +0530

    tried code of variable sliding window

--- src/Arrays/SlidingWindow/LongestSubarraySumWithSumK.java
+++ src/Arrays/SlidingWindow/LongestSubarraySumWithSumK.java
@@ -2,35 +2,32 @@ package Arrays.SlidingWindow;
 
 public class LongestSubarraySumWithSumK {
     public static void main(String[] args) {
-        int[] arr = {4,1,1,1,1,1};
+        int[] arr = {4, 1, 1, 1, 2,3,5};
         System.out.println(lenOfLongestSubarr(arr, 5));
     }
 
     public static int lenOfLongestSubarr(int[] nums, int k) {
-        int i = 0, j = 0, sum = 0, maxLen = 0;
-        // code here
-        while (i < nums.length) {
-            if (sum < k) {
-                if(j> nums.length-1) return maxLen;
-                sum += nums[j];
-                j++;
-            } else if (sum == k) {
-//                find current window length
-                int len = j - i ;
-//                store that if greater than max length
-                if (len > maxLen) {
-                    maxLen = len;
+        int i = 0, j = 0, sum = 0, max = 0;
+        while (j < nums.length) {
+            int window = j - i + 1;
+            sum += nums[i];
+//            check if sum == k
+            if (sum == k) {
+                if (sum > max) {
+                    max = window;
                 }
+                j++;
+            }
+            //            check if sum > k
+            else if (sum > k) {
                 sum -= nums[i];
                 i++;
             }
-            else{
-//                sum > k
-//                remove element from ith position
-                sum-= nums[i];
-                i++;
-            }
+//            check if sum < k
+            j++;
+
         }
-        return maxLen;
+        return max;
     }
+
 }
