5314fd19143263f7ce398cadf9a2c659fbf66b14
Jay Kumar Gupta <jayesh.gupta3224@gmail.com>
2025-06-14 11:28:40 +0530

    variable sliding window fixed code

--- src/Arrays/SlidingWindow/LongestSubarraySumWithSumK.java
+++ src/Arrays/SlidingWindow/LongestSubarraySumWithSumK.java
@@ -2,28 +2,26 @@ package Arrays.SlidingWindow;
 
 public class LongestSubarraySumWithSumK {
     public static void main(String[] args) {
-        int[] arr = {4, 1, 1, 1, 2,3,5};
+        int[] arr = {4, 1, 1, 1, 2, 3, 5};
         System.out.println(lenOfLongestSubarr(arr, 5));
     }
 
     public static int lenOfLongestSubarr(int[] nums, int k) {
         int i = 0, j = 0, sum = 0, max = 0;
         while (j < nums.length) {
-            int window = j - i + 1;
-            sum += nums[i];
-//            check if sum == k
-            if (sum == k) {
-                if (sum > max) {
-                    max = window;
-                }
-                j++;
-            }
-            //            check if sum > k
-            else if (sum > k) {
+//            find sum
+            sum += nums[j];
+            // Shrink the window from left if sum > k
+            while (sum > k && i <= j) {
                 sum -= nums[i];
                 i++;
             }
-//            check if sum < k
+//            check if sum == k
+            if (sum == k) {
+                int window = j - i + 1;
+                max = Math.max(max, window);
+            }
+//           if sum < k move j and keep adding elements
             j++;
 
         }
